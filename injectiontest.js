b5b218abdef297251f7184e8ecb5f161e633d04d
Ahmed Alhag <aalhag24@gmail.com>
2015-07-18 09:16:11 -0700

    Fixed json format

--- injectiontest.js
+++ injectiontest.js
@@ -1,6 +1,5 @@
 console.log('START injectiontest.js');
 
-
 (function ($){
   console.log('START injectiontest.js: self initializing function');
 
@@ -17,21 +16,48 @@ console.log('START injectiontest.js');
     //!!TODO WHEN THIS AJAX IS UNCOMMENTED, IT BREAKS THE ABOVE document.onmouseup = function() { //stuff }
     //!!TODO WHEN THIS AJAX IS UNCOMMENTED, IT BREAKS THE ABOVE document.onmouseup = function() { //stuff }
 
-    // $.ajax({
-    //   url: 'freedom-hack-app.herokuapp.com'
-    //   type: 'post',
-    //   content: {
-    //     text: ''
-    //   }
-    // }).done(function (body){
-    //   console.log('ajax done');
-    // }).fail(function (body){
-    //   console.log('ajax fail');
-    // }).always(function (body){
-    //   console.log('ajax always');
-    // });
+$.ajax({
+      url: 'freedom-hack-app.herokuapp.com',     
+      type: 'post',    
+      content: {
+        text: ''
+      }
+    }).done(function (body){
+      console.log('ajax done');
+    }).fail(function (body){
+      console.log('ajax fail');
+    }).always(function (body){
+      console.log('ajax always');
+    });
 
+/*
+    $.ajax({
+      url: 'freedom-hack-app.herokuapp.com'     --- add "," after line of code ---    ||    also " " instead of ' ' 
+      type: 'post',         Replace  -  type: 'post',  with  type:"POST",
+      content: {
+        text: ''
+      }
+    }).done(function (body){
+      console.log('ajax done');
+    }).fail(function (body){
+      console.log('ajax fail');
+    }).always(function (body){
+      console.log('ajax always');
+    });
+*/
 
+/*      
+                    Example code
+        $.ajax({
+          type: "POST",
+          url: url,
+          data: data,
+          success: success,
+          dataType: dataType
+        });
+
+        Reference - http://api.jquery.com/jquery.post/
+*/
 
   };
 
